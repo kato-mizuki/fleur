d85e59d3179dc6e098b86d9b393f0d8b2c01529b
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2023-09-24 13:46:07 +0000

    Top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,48 @@
 Rails.application.routes.draw do
+  namespace :admin do
+   resources :orders, only: [:show]
+  end
+  
+  namespace :admin do
+   resources :customers, only: [:index,:show,:edit,:update]
+  end
+  
+  namespace :admin do
+   resources :items
+  end
+  
+  namespace :admin do
+   root to: 'homes#top'
+  end
+  
+  namespace :public do
+   root to: 'homes#top'
+   get '/about' => 'homes#about'
+  end
+  
+  namespace :public do
+   get 'orders/thanks' => 'orders#thanks'
+   post 'orders/confirm' => 'orders#confirm'
+   resources :orders, only: [:new,:index,:create,:show]
+  end
+  
+  namespace :public do
+   delete '/cart_items' => 'cart_items#destroy_all'
+   resources :cart_items, only: [:index,:create,:update,:destroy]
+  end
+  
+  namespace :public do
+   get 'customers/information' => 'customers#show'
+   get 'customers/information/edit' => 'customers#edit'
+   patch 'customers/update' => 'customers#update'
+   get 'customers/withdraw' => 'customers#withdraw'
+   patch 'customers/withdraw' => 'customers#withdraw_update'
+  end
+  
+  namespace :public do
+   resources :items, only: [:index,:show]
+  end
+  
   devise_for :admins, skip: [:registrations, :passwords] , controllers: {
    sessions: "admin/sessions"
   } 
@@ -6,5 +50,13 @@ Rails.application.routes.draw do
    registrations: "public/registrations",
    sessions: 'public/sessions'
   }
+  
+  devise_scope :customers do
+   resources :sessions, only: [:new,:create,:destroy]
+  end
+
+  devise_scope :customers do
+   resources :registrations, only: [:new,:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20230923095451_devise_create_customers.rb
+++ db/migrate/20230923095451_devise_create_customers.rb
@@ -4,12 +4,6 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
       ## Database authenticatable
-      t.string :last_name, null :false
-      t.string :first_name, null :false
-      t.string :telephone_number, null :false
-      t.string :postal_code, null :false
-      t.string :address, null :false
-      t.boolean :is_deleted ,null :false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -39,6 +33,12 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
 
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :telephone_number, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.boolean :is_deleted, null: false
       t.timestamps null: false
 
     end
