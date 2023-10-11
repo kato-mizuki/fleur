0c5f8623e51a06f560442220b57ff4b74434da75
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2023-10-11 06:17:10 +0000

    Finish

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,25 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customer = Customer.page(params[:page]).per(10)
+  end
+
+  def show
+    @customers = Customer.find(params[:id])
+  end
+
+  def edit
+    @customers = Customer.find(params[:id])
+  end
+
+  def update
+    customers = Customer.find(params[:id])
+    customers.update(customer_params)
+    redirect_to admin_customer_path(customers.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :postal_code, :telephone_number, :address, :email, :is_deleted)
+  end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,47 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items
+    @total = 0
+  end
+
+  def thanks
+  end
+
+  def create
+    @orders = Order.new(order_params)
+    @orders.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items
+    @orders.save
+    @cart_items.each do |cart_item|
+        order_detail = OrderDetail.new()
+        order_detail.order_id = @orders.id
+        order_detail.item_id = cart_item.item_id
+        order_detail.item_amount = cart_item.amount
+        order_detail.add_tax_price = cart_item.item.with_tax_price
+        order_detail.save
+        cart_item.delete
+    end
+
+    redirect_to public_orders_thanks_path
+  end
+
+  def index
+    @orders= current_customer.orders
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @cart_items = current_customer.cart_items
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :postal_code, :address, :payment_method, :price, :postage, :id)
+  end
 end
