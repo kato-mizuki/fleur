a5c36795e2825f08a25d013b3a6e227beafe7fd3
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2023-10-29 12:18:29 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,43 @@
 Rails.application.routes.draw do
+  root "public/homes#top"
+  
   namespace :admin do
-   resources :orders, only: [:show]
-  end
-
-  namespace :admin do
-   resources :customers, only: [:index,:show,:edit,:update]
-  end
-
-  namespace :admin do
-   resources :items
-  end
-
-  namespace :admin do
-   root to: 'homes#top'
+    resources :orders, only: [:show]
+    resources :customers, only: [:index,:show,:edit,:update]
+    resources :items
+    root to: 'homes#top'
   end
 
   namespace :public do
-   root to: 'homes#top'
-   get '/about' => 'homes#about'
-  end
-
-  namespace :public do
-   get 'orders/thanks' => 'orders#thanks'
-   post 'orders/confirm' => 'orders#confirm'
-   resources :orders, only: [:new,:index,:create,:show]
-  end
-
-  namespace :public do
-   delete '/cart_items' => 'cart_items#destroy_all'
-   resources :cart_items, only: [:index,:create,:update,:destroy]
-  end
-
-   namespace :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    get 'orders/thanks' => 'orders#thanks'
+    post 'orders/confirm' => 'orders#confirm'
+    resources :orders, only: [:new,:index,:create,:show]
+    delete '/cart_items' => 'cart_items#destroy_all'
+    resources :cart_items, only: [:index,:create,:update,:destroy]
     get 'customers/information' => 'customers#show'
     get 'customers/information/edit' => 'customers#edit'
     patch 'customers/update' => 'customers#update'
     get 'customers/withdraw' => 'customers#withdraw'
     patch 'customers/withdraw' => 'customers#withdraw_update'
-  end
-
-
-  namespace :public do
-   resources :items, only: [:index,:show] do
+    resources :items, only: [:index,:show] do
     resource :favorites,only:[:create,:destroy]
-   end
+    end
   end
 
   devise_for :admins, skip: [:registrations, :passwords] , controllers: {
-   sessions: "admin/sessions"
+    sessions: "admin/sessions"
   }
+  
   devise_for :customers,skip: [:passwords], controllers: {
-   registrations: "public/registrations",
-   sessions: 'public/sessions'
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
   }
 
   devise_scope :customers do
-   resources :sessions, only: [:new,:create,:destroy]
-  end
-
-  devise_scope :customers do
-   resources :registrations, only: [:new,:create]
+    resources :sessions, only: [:new,:create,:destroy]
+    resources :registrations, only: [:new,:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
